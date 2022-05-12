6ddfd5386718d637853acf0935556b31f1bcf677
konakona <konakona@crazyphper.com>
2022-05-12 16:37:43 +0800

    正确的MP插件开启配置

--- src/main/java/com/example/ME/DEMO/config/MybatisPlusConfiguration.java
+++ src/main/java/com/example/ME/DEMO/config/MybatisPlusConfiguration.java
@@ -1,5 +1,18 @@
+/*
+ * @Author: konakona konakona@crazyphper.com
+ * @Date: 2022-05-11 11:32:26
+ * @LastEditors: konakona konakona@crazyphper.com
+ * @LastEditTime: 2022-05-12 16:36:25
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by konakona konakona@crazyphper.com, All Rights Reserved. 
+ */
 package com.example.ME.DEMO.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,10 +21,15 @@ public class MybatisPlusConfiguration {
     /**
      * 乐观锁插件配置
      * 
-     * @return OptimisticLockerInnerInterceptor
+     * @return MybatisPlusInterceptor
      */
     @Bean
-    public OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor() {
-        return new OptimisticLockerInnerInterceptor();
+    public MybatisPlusInterceptor innerInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 添加乐观锁
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        // 添加Sql的分页拦截器
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
     }
 }
