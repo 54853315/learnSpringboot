1b25f870aa7cdc54b93ec7b03db63654168d51e5
konakona <konakona@crazyphper.com>
2022-05-25 15:06:23 +0800

    common 目录结构

---
+++ common/src/main/java/pers/learn/exception/ApiException.java
@@ -0,0 +1,25 @@
+package pers.learn.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+public class ApiException extends RuntimeException {
+    private String code;
+    private String message;
+
+    public ApiException(String message) {
+        this.code = HttpStatus.UNPROCESSABLE_ENTITY.toString();
+        this.message = message;
+    }
+
+    public ApiException(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
