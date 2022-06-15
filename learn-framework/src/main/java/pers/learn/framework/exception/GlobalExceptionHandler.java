2e40bfd8d428aac5c030c3f659fefb491e4db545
konakona <konakona@crazyphper.com>
2022-06-15 15:20:20 +0800

    增加日志的内容

--- learn-framework/src/main/java/pers/learn/framework/exception/GlobalExceptionHandler.java
+++ learn-framework/src/main/java/pers/learn/framework/exception/GlobalExceptionHandler.java
@@ -9,14 +9,12 @@
  */
 package pers.learn.framework.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.javassist.NotFoundException;
-import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.ShiroException;
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.apache.shiro.session.ExpiredSessionException;
-import org.apache.shiro.subject.Subject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,10 +23,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pers.learn.common.exception.ApiException;
 import pers.learn.common.response.CommonResponse;
 
+/**
+ * 全局捕获异常类，只要作用在@RequestMapping上的所有异常都会被捕获
+ */
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     /**
      * @param {*} e
@@ -39,23 +39,35 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public CommonResponse<String> runtimeException(Exception e) {
-        logger.error("捕获RuntimeExcept："+e.getMessage(), e);
+        log.error("进入runtimeException：" + e.getMessage(), e);
         return CommonResponse.fail(e.getMessage());
     }
+
 
     // 当用户访问未经授权的资源时，如果没有主动捕获，统一返回这条
     @ExceptionHandler(value = {AuthorizationException.class, UnauthorizedException.class})
     public CommonResponse<String> authorizationException(AuthorizationException e) {
-        logger.error(e.getMessage());
-        Subject subject = SecurityUtils.getSubject();
-        if(subject.isAuthenticated()){
-            return CommonResponse.fail("您已登录过了");
-        }
+        log.error("进入authorizationException:" + e);
+        // 这个subject只有在启用shiro session的情况下会被需要
+//        Subject subject = SecurityUtils.getSubject();
+//        if (subject.isAuthenticated()) {
+//            return CommonResponse.fail("您已登录过了");
+//        }
         return CommonResponse.fail("无授权，不可访问");
     }
 
+    // 捕捉shiro的异常
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(ShiroException.class)
+    public CommonResponse<String> shiroException(ShiroException e) {
+        log.error("进入shiroException:" + e.getMessage());
+        return CommonResponse.fail(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = {ExpiredSessionException.class})
-    public CommonResponse<String> expiredSessionException(AuthorizationException e) {
+    public CommonResponse<String> expiredSessionException(ExpiredSessionException e) {
+        log.error("进入expiredSessionException:" + e.getMessage());
         return CommonResponse.fail("授权已过期，请重新登录");
     }
 
@@ -63,17 +75,21 @@ public class GlobalExceptionHandler {
     // 当用户访问需要RequestBody的资源却没有携带时，统一返回这条
     @ExceptionHandler(value = {HttpMessageNotReadableException.class})
     public CommonResponse<Object> httpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error("进入httpMessageNotReadableException:" + e.getMessage());
         return CommonResponse.fail("request body不能为空");
     }
 
     // 将Api异常捕获并格式化输出
     @ExceptionHandler(ApiException.class)
     public CommonResponse<Object> apiException(ApiException e) {
-        return CommonResponse.fail(CommonResponse.Type.valueOf(e.getCode().toString()), e.getMessage());
+        log.error("进入apiException:" + e.getMessage());
+//        return CommonResponse.fail(CommonResponse.Type.valueOf(e.getCode().toString()), e.getMessage());
+        return CommonResponse.fail(e.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
     public CommonResponse<Object> notFoundException(Exception e) {
+        log.error("进入notFoundException:" + e.getMessage());
         return CommonResponse.fail(CommonResponse.Type.NOT_FOUND, e.getMessage());
     }
 }
